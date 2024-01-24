69a42850c52c16bfa2c0cf1f0257ff7339bb1b1c
jacalvar <jacalvar@microsoft.com>
2024-01-24 14:37:31 +0100

    Initial files

---
+++ AngularBlazorCustomElementsSample.Server/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
